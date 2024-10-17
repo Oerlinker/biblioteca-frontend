8b29edd8584766d99af36ea8cb0e255cb415651e
Arturo Aliaga <jaav2801@gmail.com>
2024-10-16 22:03:45 -0400

    Login top

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}', 
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightblue: {
+          500: '#90c8f8', 
+          600: '#77b5f0', 
+        },
+        gray: {
+          900: '#1a1a1a', 
+          500: '#8a8a8a', 
+          300: '#d1d1d1', 
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};

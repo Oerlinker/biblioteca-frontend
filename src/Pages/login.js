1eafb12fe2c9ce4a783fa2ec07d86fcd31b90473
Oerlinker <oerlinker@gmail.com>
2024-10-29 23:50:02 -0400

    las bolas llenas

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,7 +1,7 @@
 // src/Pages/login.js
 import React, { useState, useContext } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import jwtDecode from 'jwt-decode';
+import {jwtDecode} from 'jwt-decode';
 import axios from 'axios';
 import { UserContext } from '../UserContext';
 import fondo from '../assets/fondo.jpeg';

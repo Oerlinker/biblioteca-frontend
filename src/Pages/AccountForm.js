5a0cbb24d47f6ee77573885558314b7d3ad40912
Oerlinker <oerlinker@gmail.com>
2024-10-30 18:19:19 -0400

    name accountform

--- src/Pages/AccountForm.js
+++ src/Pages/AccountForm.js
@@ -1,24 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import {jwtDecode} from 'jwt-decode';
+import { UserContext } from '../UserContext';
 import UserInfo from './UserInfo';
 import fondo from '../assets/fondo.jpeg';
 
 const AccountForm = () => {
-    const [user, setUser] = useState(null);
-
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            const decodedToken = jwtDecode(token);
-            setUser({
-                id: decodedToken.id,
-                nombre: decodedToken.nombre,
-                correo: decodedToken.correo,
-                rol: decodedToken.rol
-            });
-        }
-    }, []);
+    const { user } = useContext(UserContext);
 
     return (
         <div className="relative min-h-screen flex items-center justify-center bg-gray-100">
@@ -29,7 +16,7 @@ const AccountForm = () => {
             <div className="relative flex w-full max-w-6xl mx-auto z-10 bg-white rounded-3xl shadow-2xl overflow-hidden">
                 <aside className="w-1/3 bg-gray-100 p-8 flex flex-col items-center justify-center rounded-l-3xl">
                     {user ? (
-                        <UserInfo user={user} />
+                        <UserInfo />
                     ) : (
                         <p className="text-gray-600">Cargando información del usuario...</p>
                     )}

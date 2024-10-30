3adbff1fa0e59780f1ad2c41a212535392e848af
Richiculo <rvargasosinaga@gmail.com>
2024-10-30 05:46:31 -0400

    Reseña 1

--- src/Pages/GestionarPrestamos.js
+++ src/Pages/GestionarPrestamos.js
@@ -6,6 +6,14 @@ import moment from 'moment-timezone';
 const GestionarPrestamos = () => {
     const [prestamos, setPrestamos] = useState([]);
     const [user, setUser] = useState(null); 
+    const [isReviewFormVisible, setIsReviewFormVisible] = useState(false);
+    const [successMessage, setSuccessMessage] = useState('');
+    const [review, setReview] = useState({
+    edicionid: null,
+    libroid: null,
+    calificacion: 0,
+    comentario: '',
+});
 
     useEffect(() => {
         const fetchUser = () => {
@@ -34,7 +42,7 @@ const GestionarPrestamos = () => {
 
     useEffect(() => {
         fetchPrestamos();
-    }, [fetchPrestamos]); // Dependencia de fetchPrestamos
+    }, [fetchPrestamos]);
 
     const handleDevolucion = async (prestamoid) => {
         try {
@@ -48,9 +56,53 @@ const GestionarPrestamos = () => {
         }
     };
 
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setReview((prevReview) => ({
+            ...prevReview,
+            [name]: value,
+        }));
+    };
+    
+    const handleSubmitReview = async (e) => {
+        e.preventDefault();
+        console.log('reseña',review);
+        try {
+            await axios.post(`https://backend-proyecto-production-13fc.up.railway.app/api/users/review`, {
+                miembroid: user.miembroid,
+                edicionid: review.edicionid,
+                libroid: review.libroid,
+                calificacion: review.calificacion,
+                comentario: review.comentario,
+            });
+            // Reiniciar el formulario
+            setReview({ edicionid: null, libroid: null, calificacion: 0, comentario: '' });
+            setIsReviewFormVisible(false);
+            fetchPrestamos(); // Para actualizar la lista de préstamos si es necesario
+            setSuccessMessage('¡Reseña enviada con éxito!');
+            setTimeout(() => {
+                setSuccessMessage('');
+            }, 3000); 
+        } catch (error) {
+            console.error('Error al enviar la reseña:', error);
+        }
+    };
+
     return (
         <div style={{ padding: '20px', maxWidth: '600px', margin: 'auto' }}>
             <h2 style={{ textAlign: 'center' }}>Préstamos Activos</h2>
+            {successMessage && (
+                <div style={{
+                    backgroundColor: '#d4edda',
+                    color: '#155724',
+                    padding: '10px',
+                    borderRadius: '5px',
+                    marginBottom: '15px',
+                    textAlign: 'center',
+                }}>
+                    {successMessage}
+                </div>
+            )}
             {prestamos.length > 0 ? (
                 <ul style={{ listStyleType: 'none', padding: 0 }}>
                     {prestamos.map((prestamo) => (
@@ -59,30 +111,85 @@ const GestionarPrestamos = () => {
                             marginBottom: '10px',
                             border: '1px solid #ccc',
                             borderRadius: '5px',
-                            backgroundColor: '#f9f9f9',
-                            display: 'flex', // Habilita flexbox
-                            justifyContent: 'space-between', // Espacio entre los elementos
-                            alignItems: 'center' // Centra verticalmente
+                            backgroundColor: '#f9f9f9'
                         }}>
-                            <div>
-                                <h4 style={{ margin: '0' }}>{prestamo.titulo}</h4>
-                                <p style={{ margin: '5px 0' }}>
-                                    <strong>Número de Edición:</strong> {prestamo.numero_edicion}
-                                </p>
-                                <p style={{ margin: '5px 0' }}>
-                                    <strong>Fecha de Devolución:</strong> {moment(prestamo.fecha_devolucion).format('DD/MM/YYYY')}
-                                </p>
+                            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <div>
+                                    <h4 style={{ margin: '0' }}>{prestamo.titulo}</h4>
+                                    <p style={{ margin: '5px 0' }}>
+                                        <strong>Número de Edición:</strong> {prestamo.numero_edicion}
+                                    </p>
+                                    <p style={{ margin: '5px 0' }}>
+                                        <strong>Fecha de Devolución:</strong> {moment(prestamo.fecha_devolucion).format('DD/MM/YYYY')}
+                                    </p>
+                                </div>
+                                <div style={{ display: 'flex', gap: '10px' }}>
+                                    <button onClick={() => handleDevolucion(prestamo.prestamoid)} style={{
+                                        backgroundColor: '#007BFF',
+                                        color: 'white',
+                                        border: 'none',
+                                        borderRadius: '5px',
+                                        padding: '10px 15px',
+                                        cursor: 'pointer',
+                                    }}>
+                                        Devolver
+                                    </button>
+                                    <button onClick={() => {
+                                        setReview((prevReview) => {
+                                            const updatedReview = {
+                                                ...prevReview,
+                                                edicionid: prestamo.edicionid,
+                                                libroid: prestamo.libroid,
+                                            };
+                                            console.log('Updated review state:', updatedReview);
+                                            return updatedReview;
+                                        });
+                                        setIsReviewFormVisible(isReviewFormVisible === prestamo.prestamoid ? null : prestamo.prestamoid);
+                                    }} style={{
+                                        backgroundColor: '#28A745',
+                                        color: 'white',
+                                        border: 'none',
+                                        borderRadius: '5px',
+                                        padding: '10px 15px',
+                                        cursor: 'pointer',
+                                    }}>
+                                        {isReviewFormVisible === prestamo.prestamoid ? 'Cancelar Reseña' : 'Hacer Reseña'}
+                                    </button>
+                                </div>
                             </div>
-                            <button onClick={() => handleDevolucion(prestamo.prestamoid)} style={{
-                                backgroundColor: '#007BFF',
-                                color: 'white',
-                                border: 'none',
-                                borderRadius: '5px',
-                                padding: '10px 15px',
-                                cursor: 'pointer',
-                            }}>
-                                Devolver
-                            </button>
+                            {/* Formulario de reseña para el préstamo seleccionado */}
+                            {isReviewFormVisible === prestamo.prestamoid && (
+                                <form onSubmit={handleSubmitReview} style={{ marginTop: '10px' }}>
+                                    <input
+                                        type="number"
+                                        name="calificacion"
+                                        step="0.1"
+                                        min="1"
+                                        max="5"
+                                        placeholder="Calificación (1.0 - 5.0)"
+                                        value={review.calificacion}
+                                        onChange={handleInputChange}
+                                        required
+                                        style={{ marginRight: '10px' }}
+                                    />
+                                    <textarea
+                                        name="comentario"
+                                        placeholder="Escribe tu comentario"
+                                        value={review.comentario}
+                                        onChange={handleInputChange}
+                                        required
+                                        style={{ display: 'block', margin: '10px 0', width: '100%' }}
+                                    />
+                                    <button type="submit" style={{
+                                        backgroundColor: '#28A745',
+                                        color: 'white',
+                                        border: 'none',
+                                        borderRadius: '5px',
+                                        padding: '10px 15px',
+                                        cursor: 'pointer'
+                                    }}>Enviar Reseña</button>
+                                </form>
+                            )}
                         </li>
                     ))}
                 </ul>

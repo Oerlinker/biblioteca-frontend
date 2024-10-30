e7a8f8166e0e0df5e85cf170d145654ddafa0818
Oerlinker <oerlinker@gmail.com>
2024-10-30 17:04:03 -0400

    header 2.O

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,9 @@ const Header = ({ handleLogout }) => {
                 {isLoggedIn && (
                     <div className="relative">
                         <button onClick={toggleDropdown} className="text-lg font-semibold text-white focus:outline-none">
-                            Bienvenido, {user.nombre}
+                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                            </svg>
                         </button>
                         {isDropdownOpen && (
                             <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2 z-20">

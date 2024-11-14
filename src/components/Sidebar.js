1d1b55d91cb51c9d171e4d593426bb1ae4cd427d
Oerlinker <oerlinker@gmail.com>
2024-11-13 21:04:48 -0400

    fix sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,9 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { UserContext } from '../UserContext';
 import Logo from '../assets/logo.png';
 
 const Sidebar = () => {
-    const { user } = useContext(UserContext);
+
     const [isAdminOpen, setIsAdminOpen] = useState(false);
     const [isHovered, setIsHovered] = useState(false);
 
